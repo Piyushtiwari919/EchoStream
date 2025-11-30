7dd53024eac04b389f202cabb57e8568f108110d
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-11-30 23:00:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center w-dvw h-dvh bg-[#333]">
+      <h1 className="text-4xl text-amber-950 bg-amber-300 rounded-2xl p-4 m-4">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
