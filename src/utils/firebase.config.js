b0287f693f0f0b38a7bddc88a52920efe354418b
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-12-02 00:45:28 +0530

    feat: update Firebase configuration to use environment variable for API key

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBxK8sNGEBT_ngYtkLZif0RyEocrRFYWLU",
+  apiKey: process.env.GOOGLE_API_KEY,
   authDomain: "netflixgpt-98344.firebaseapp.com",
   projectId: "netflixgpt-98344",
   storageBucket: "netflixgpt-98344.firebasestorage.app",
