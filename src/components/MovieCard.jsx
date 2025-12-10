22b147aac51cc2f66831c673914588b4e6f689ee
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-12-10 17:39:04 +0530

    feat: enhance MovieCard and MovieList components with improved styling and layout adjustments

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,8 @@ import { IMG_CDN } from "../utils/constants";
 
 const MovieCard = ({posterPath,movieTitle})=>{
     return(
-    <div className="w-40 h-6">
-        <img alt={movieTitle} src={IMG_CDN+posterPath}/>
+    <div className="w-40 rounded-lg">
+        <img alt={movieTitle} src={IMG_CDN+posterPath} className="rounded-lg"/>
     </div>
 )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,24 @@
 import MovieCard from "./MovieCard.jsx";
 
-const MovieList = ({title, movies})=>{
-    console.log(movies);
-    if(!movies || movies.length===0) return;
-    return(
-        <div>
-            <h1>{title}</h1>
-            <div>
-                <MovieCard posterPath={movies[0]?.poster_path} movieTitle={movies[0]?.original_title}/>
-            </div>
-        </div>
-    )
-}
+const MovieList = ({ title, movies }) => {
+  console.log(movies);
+  if (!movies || movies.length === 0) return;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl m-4 font-bold">{title}</h1>
+      <div className="flex flex-wrap m-4 gap-2 items-center justify-center">
+        {movies.map((movie) => {
+          return (
+            <MovieCard
+              key={movie.id}
+              posterPath={movie.poster_path}
+              movieTitle={movie.original_title}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default MovieList;
